e55b49193d1db076fd5fef6ed279174c6240a373
celadii413 <celadii413@gmail.com>
2025-04-22 08:27:59 +0700

    Add project files.

---
+++ CoffeeShop/Models/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.Models.Services;
+
+namespace CoffeeShop.Models.Interfaces
+{
+    public interface IProductRepository 
+    {
+        IEnumerable<Product> GetAllProducts();
+        IEnumerable<Product> GetTrendingProducts();
+        Product GetProductDetail(int id);
+    }
+}
